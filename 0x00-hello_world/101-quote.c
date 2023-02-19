77e3b6f6abc552767fd766d2fd34ce17e975e4ae
Theodorah <rikhotsotheodora@gmail.com>
2023-02-19 03:49:16 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - Entry point
  * Return: Always 1
  */
-
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora korpar, 2015-19\n", 59);
