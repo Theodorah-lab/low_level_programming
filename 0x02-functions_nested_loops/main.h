9e838f673bb3ee634e1bd69fe1e149f4d5ebf623
Theodorah <rikhotsotheodora@gmail.com>
2023-02-22 13:13:31 -0800

    10-add.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,6 +3,8 @@
 
 int _putchar(char c);
 
+#endif /* MAIN_H */
+
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -12,5 +14,4 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-void print_to_98(int n);
-void print_times_table(int n);
+int add(int, int);
