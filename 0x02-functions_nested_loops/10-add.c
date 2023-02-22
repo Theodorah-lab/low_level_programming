b925278224bde5468c5dd81b6a6b53dcdec38093
Theodorah <rikhotsotheodora@gmail.com>
2023-02-22 12:33:40 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * add - adds three integers
+ * @f1: integer 1
+ * @f2: integer 2
+ * @f3: integer 3
+ *
+ * Description: give three integers, returns sum
+ *
+ * Return: sum
+ */
+
+int add(int f1, int f2)
+{
+	int sum;
+
+	sum = f1 + f2 + f3;
+
+	return (sum);
+}
