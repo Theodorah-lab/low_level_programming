28f90022f59898b12b1e8874a6dc7435aa2162da
Theodorah <rikhotsotheodora@gmail.com>
2023-02-22 13:35:39 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,30 +3,30 @@
 
 /**
  * print_to_98 - prints numbers to 98
- * @f: starting integer
+ * @n: starting integer
  *
- * Description: prints all natural number from f - 98
+ * Description: prints all natural number from n - 98
  *
  * Return: void
  */
 
-void print_to_98(int f)
+void print_to_98(int n)
 {
-	if (f < 98)
+	if (n < 98)
 	{
-		while (f < 98)
+		while (n < 98)
 		{
-			printf("%d, ", f);
-			f++;
+			printf("%d, ", n);
+			n++;
 		}
 	}
-	else if (f > 98)
+	else if (n > 98)
 	{
-		while (f > 98)
+		while (n > 98)
 		{
-			printf("%d, ", f);
-			f--;
+			printf("%d, ", n);
+			n--;
 		}
 	}
-	printf("98\f");
+	printf("98\n");
 }
