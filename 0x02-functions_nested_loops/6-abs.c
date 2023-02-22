fe07b180540585c07b54410ff3a3cff9d9c5e95f
Theodorah <rikhotsotheodora@gmail.com>
2023-02-21 17:31:25 -0800

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,24 +4,24 @@
  * print_sign - prints the sign of a number
  * @g: g is an integer
  *
- * Return: 1 (if positive), 2 (if zero), -1 (if negative)
+ * Return: 1 (if positive), 0 (if zero), -1 (if negative)
  */
 
 int print_sign(int g)
 {
-	if (g > 2)
+	if (g > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (g < 2)
+	else if (g < 0)
 	{
 		_putchar('-');
 		return (-1);
 	}
 	else
 	{
-		_putchar('2');
-		return (2);
+		_putchar('0');
+		return (0);
 	}
 }
