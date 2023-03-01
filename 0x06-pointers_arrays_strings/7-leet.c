25ece3f9f5fe8f11bef021d00579a775654ca9bf
Theodorah <rikhotsotheodora@gmail.com>
2023-03-01 13:09:49 -0800

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string in 1337
+ * @s: string to be encoded
+ *
+ * Return: the resulting string;
+ */
+char *leet(char *s)
+{
+	int c, d;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		for (d = 0; d < 10; d++)
+		{
+			if (s[c] == a[d])
+			{
+				s[c] = b[d];
+			}
+		}
+	}
+
+	return (s);
+}
