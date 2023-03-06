ca4d8cfcd3968b74a05c2f081ae54027dba95954
Theodorah <rikhotsotheodora@gmail.com>
2023-03-06 09:14:38 -0800

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	char *s = "hello, world";
+	char *p = "helloIhello";
+	char *f = "oleh";
+	unsigned int m, z;
+
+	n = _strspn(s, f);
+	z = _strspn(p, f);
+	printf("%u\n", m);
+	printf("%u\n", z);
+	return (0);
+}
