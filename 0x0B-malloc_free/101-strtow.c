9a12eab350c57bd9082ec23b6d27a58478c787c0
Theodorah <rikhotsotheodora@gmail.com>
2023-03-15 12:46:36 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,41 +1,101 @@
-#include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * print_tab - Prints an array of string
+ * wordcount - get word count from string
+ *             without spaces
  *
- * @tab: The array to print
+ * @str: string to count words present
+ *
+ * Return: The number of words
+*/
+
+int wordcount(char *str)
+{
+	int words = 0;
+
+	while (*str != '\0')
+	{
+		/*skip spaces*/
+		if (*str == ' ')
+			str++;
+		else
+		{
+			/*count words*/
+			while (*str != ' ' && *str != '\0')
+				str++;
+			words++;
+		}
+	}
+	return (words);
+}
+
+/**
+ * free_array - free arr[i]
+ *
+ * @ar: array to free
+ * @i: array[i]
  *
  * Return: nothing
 */
 
-void print_tab(char **tab)
+void free_array(char **ar, int i)
 {
-	int i;
-
-	for (i = 0; tab[i] != NULL; ++i)
+	if (ar != NULL && i != 0)
 	{
-		printf("%s\n", tab[i]);
+		while (i >= 0)
+		{
+			free(ar[i]);
+			i--;
+		}
+		free(ar);
 	}
 }
 
 /**
- * main - check the code for ALX School students.
+ * strtow - split a string to words
  *
- * Return: 1 if an error occurred, 0 otherwise
+ * @str: string to split.
+ *
+ * Return: NULL if it fails
 */
 
-int main(void)
+char **strtow(char *str)
 {
-	char **tab;
+	int i, s, j, str_l, word;
+	char **string;
 
-	tab = strtow("      Holberton School         #cisfun      ");
-	if (tab == NULL)
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	str_l = wordcount(str);
+	/*return null if str_l == 0 || new == NULL*/
+	string = malloc((str_l + 1) * sizeof(char *));
+	if (str_l == 0 || string == NULL)
+		return (NULL);
+
+	for (i = s = 0; i < str_l; i++)
 	{
-		printf("Failed\n");
-		return (1);
+		for (word = s; str[word] != '\0'; word++)
+		{
+			if (str[word] == ' ')
+				s++;
+
+			if (str[word] != ' ' && (str[word + 1] == ' ' || str[word + 1] == '\0'))
+			{
+				string[i] = malloc((word - s + 2) * sizeof(char));
+				if (string[i] == NULL)
+				{
+					free_array(string, i);
+					return (NULL);
+				}
+				break;
+			}
+		}
+
+		for (j = 0; s <= word; s++, j++)
+			string[i][j] = str[s];
+		string[i][j] = '\0';
 	}
-	print_tab(tab);
-	return (0);
+	string[i] = NULL;
+	return (string);
 }
