2783ad01c7fb85ef4e4b562257e1394134727619
Theodorah <rikhotsotheodora@gmail.com>
2023-04-10 12:17:28 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,14 +6,14 @@
  * @index: index starting from 0, of the bit we want to get
  * Return: Value of bit at index, or -1 if error
  */
-int get_bit(unsigned long int m, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int hold;
 
 	if (index > 64)
 		return (-1);
 
-	hold = m >> index;
+	hold = n >> index;
 
 	return (hold & 1);
 }
